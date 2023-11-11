e257b216ff3fc7e8f923562d2bff83530bdbeb26
Chansoo Park <chansoopark@192.168.nate.com>
2023-11-12 03:13:53 +0900

    Fix: done2

--- src/components/pages/signup/signup.queries.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const SIGN_UP_MUTATION = gql`
-  mutation createUser($createUserDTO: CreateUserDTO!) {
-    createUser(createUserDTO: $createUserDTO) {
-      accessToken
-    }
-  }
-`;
