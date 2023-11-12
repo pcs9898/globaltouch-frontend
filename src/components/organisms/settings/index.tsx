0efd8932030ffe35d74e4f259e6915cf21f1352a
Chansoo Park <chansoopark@Chansoos-MacBook-Air.local>
2023-11-12 23:45:26 +0900

    Fix: done2

--- src/components/organisms/settings/index.tsx
+++ src/components/organisms/settings/index.tsx
@@ -48,7 +48,7 @@ export default function Settings({ drawerOnClose }: ISettingsProps) {
     setUserLoggedInInfo(null);
     Cookie.remove("refreshToken");
     document.cookie =
-      "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.channitest.shop; SameSite=None; Secure";
+      "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=channitest.shop; SameSite=None; Secure";
 
     if (drawerOnClose) {
       drawerOnClose();
