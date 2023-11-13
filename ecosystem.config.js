fe0514a794ae6c5d820c2621d80b8eaace414850
Chansoo Park <chansoopark@Chansoos-MacBook-Air.local>
2023-11-14 00:34:39 +0900

    Fix: done2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,6 @@ module.exports = {
     {
       name: "globaltouch-frontend",
       script: "node_modules/next/dist/bin/next",
-      exec_mode: "cluster",
-      instances: 0,
       autorestart: true,
     },
   ],
